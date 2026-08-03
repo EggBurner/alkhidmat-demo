a6a6163aed09bee339590f49407f73674dc96611
Abdullah Islam <itsabddev@gmail.com>
2026-08-03 14:20:21 +0500

    added header

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderBar from './HeaderBar';
+
+const Hero = () => {
+  return (
+    <div className="h-90 bg-linear-to-r from-[#000E0D] to-[#007468] pt-6">
+      <HeaderBar />
+    </div>
+  );
+};
+
+export default Hero;
