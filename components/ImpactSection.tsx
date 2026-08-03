e21f2849cb723153d1bd557778853e352a331d6f
Abdullah Islam <itsabddev@gmail.com>
2026-08-03 16:35:17 +0500

    added bano qabil impact section

---
+++ components/ImpactSection.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ImpactCard from './impactCard';
+
+const data: [string, string][] = [
+  ['1.M', 'Registered Students'],
+  ['157', 'BQ IT Centers'],
+  ['98k+', 'Graduated Students'],
+  ['61', 'Districts Nationwide'],
+];
+
+const ImpactSection = () => {
+  return (
+    <div className="mt-10 bg-[##F5FFF8]">
+      <h2 className="text-center text-4xl font-semibold">
+        Bano Qabil <span className="text-[#2c8e84]">Impact</span>
+      </h2>
+      <p className="mt-4 text-center text-[12px]">
+        Creating measurable impact by equipping Pakistan’s youth with
+        industry-ready IT skills.
+      </p>
+      <div className="mx-auto mt-10 grid w-[90%] grid-cols-4 gap-5">
+        {data.map((entry, index) => (
+          <ImpactCard key={index} text1={entry[0]} text2={entry[1]} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImpactSection;
